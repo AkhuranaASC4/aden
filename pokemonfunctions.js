68e37ca0c4fb8a4942f640a745a91eaa5f76162c
AkhuranaASC4 <aden.khurana@mastersny.org>
2017-07-12 13:27:25 -0400

    This is the new file

---
+++ pokemonfunctions.js
@@ -0,0 +1,21 @@
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabetLength = alphabet.length
+function randLetter(index) {
+    var randomIndex = Math.random() * index;
+    var result = Math.floor(randomIndex);
+    return result;
+}
+
+console.log(alphabet[randLetter(alphabet.length)]);
+
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabetLength = alphabet.length
+function randWord(index) {
+    var randomIndex = Math.random() * index;
+    var result = Math.floor(randomIndex);
+    return result;
+}
+
+console.log(alphabet[randWord(alphabet.length)]);
+
