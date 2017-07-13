e6b961b864b2b309d172f94367bedbb99bf79db1
AkhuranaASC4 <aden.khurana@mastersny.org>
2017-07-13 12:02:44 -0400

    Pokemon Roster

---
+++ pokemon-roster.js
@@ -0,0 +1,10 @@
+var pokemonRoster =  [
+["Charizard" , "Fire", 78, 78, 84, False],
+["Pikachu" , "Electric" , 35, 40, 55, False],
+["Genesect" , "Steel" , 71, 95, 120, True],
+["Sandslash" , "Ground" , 75, 110, 100, False],
+["Metagross" , "Steel" , 80, 130, 135, False],
+["Rayquaza", "Flying", 105, 90, 150, True],
+]
+
+
