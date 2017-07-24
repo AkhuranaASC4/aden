5f82992d8138eea30e6847b0dff0190926f33889
AkhuranaASC4 <aden.khurana@mastersny.org>
2017-07-24 12:02:38 -0400

    Monday Week2

--- aden-spaceinvaders/sketch.js
+++ aden-spaceinvaders/sketch.js
@@ -1,8 +1,12 @@
 var s = 50;
 var x = 550;
 var y = 750;
+bulletx = 550;
+bullety = 750
 var cls= 12;
 var s2 = 150;
+var notfired = true;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,14 +14,24 @@ function setup() {
 }
 
 function draw() {
-  if (keyIsDown(LEFT_ARROW))
+  if (keyIsDown(LEFT_ARROW)&& x>0)
     x-=10;
 
-  if (keyIsDown(RIGHT_ARROW))
+  if (keyIsDown(RIGHT_ARROW)&& x<1170)
     x+=10;
 
   clear();
   background('black')
+
+ellipse(bulletx,bullety,10,10)
+
+if(notfired == false){
+    bullety-=10;
+}
+
+if(bullety < 0){
+    bullety = 750
+}
 
 for (i =1; i <= cls; i++){
     rect (i*100, s, s, s)
@@ -30,10 +44,21 @@ for (i =1; i <= cls; i++){
   fill(255, 0, 0);
  rect(x,y,200,20)
 
-
-
-
 }
+
+function keyPressed(){
+   if (keyCode == 32){
+    //    bullety -=10
+    notfired = false;
+    
+   }
+}
+   
+  
+ 
+
+
+
 
 
 // fill(255,0,0);
